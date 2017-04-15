8aa264ac3e9ca11b1eb8329b1f72e3f5ee527925
Cian Hughes <cian.hughes55@mail.dcu.ie>
2017-04-15 18:42:38 +0100

    timetable now redirects to the dcu timetable search page

--- StudentLife_DjangoHome/StudentLifeApp/views.py
+++ StudentLife_DjangoHome/StudentLifeApp/views.py
@@ -19,21 +19,4 @@ def buildings(request):
 def staff(request):
     return render(request, 'StudentLifeApp/staff.html')
     
-def urlgen():
-    default_dcu_tt = 'https://www101.dcu.ie/timetables/feed.php?prog=_&per=_&_&hour=1-28&template=student'
-    
-    default_dcu_tt = default_dcu_tt.split('_')
-    
-    sem1 = 'week1=1&week2=12'
-    sem2 = 'week1=20&week2=31'
-    
-    if sem == '1':
-        sem = sem1
-    else:
-        sem = sem2
-
-    url = ''
-    url = default_dcu_tt[0] + prog + default_dcu_tt[1] + year + default_dcu_tt[2] + sem + default_dcu_tt[3]
-    return url
-    
 
