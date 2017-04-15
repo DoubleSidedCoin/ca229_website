c83d6362762d46cfde3afbc253fbd884ed14d4d3
Cian Hughes <cian.hughes55@mail.dcu.ie>
2017-04-15 13:53:01 +0100

    modified:   StudentLife_DjangoHome/settings.py 	modified:   StudentLife_DjangoHome/settings.pyc 	new file:   media/rango.jpg 	new file:   static/images/rango.jpg 	new file:   templates/StudentLifeApp/timetable.html 	../Python_scripts_test/

--- StudentLife_DjangoHome/StudentLife_DjangoHome/settings.py
+++ StudentLife_DjangoHome/StudentLife_DjangoHome/settings.py
@@ -14,7 +14,6 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print BASE_DIR
 
 PROJECT_PATH = os.path.join(BASE_DIR, os.pardir)
 
