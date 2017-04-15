de1821788c60ec7c33008c61a92a5b39bed7f1d2
Cian Hughes <cian.hughes55@mail.dcu.ie>
2017-04-15 18:36:55 +0100

    decided against using timetable search python script as importing user input from the html file proved too difficult. The script can still be found at StudentLife_DjangoHome/Python_scripts_test timetables now redirects to the dcu timetables page

--- StudentLife_DjangoHome/StudentLifeApp/views.py
+++ StudentLife_DjangoHome/StudentLifeApp/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.http import HttpResponseRedirect
+
 def index(request):
     return render(request, 'StudentLifeApp/index.html')
    
 def timetable(request):
-    return render(request, 'StudentLifeApp/timetable.html')
+    return HttpResponseRedirect('https://www.dcu.ie/registry/timetables.shtml#class')
     
 def profile(request):
     return render(request, 'StudentLifeApp/profile.html')
@@ -16,3 +18,22 @@ def buildings(request):
     
 def staff(request):
     return render(request, 'StudentLifeApp/staff.html')
+    
+def urlgen():
+    default_dcu_tt = 'https://www101.dcu.ie/timetables/feed.php?prog=_&per=_&_&hour=1-28&template=student'
+    
+    default_dcu_tt = default_dcu_tt.split('_')
+    
+    sem1 = 'week1=1&week2=12'
+    sem2 = 'week1=20&week2=31'
+    
+    if sem == '1':
+        sem = sem1
+    else:
+        sem = sem2
+
+    url = ''
+    url = default_dcu_tt[0] + prog + default_dcu_tt[1] + year + default_dcu_tt[2] + sem + default_dcu_tt[3]
+    return url
+    
+
